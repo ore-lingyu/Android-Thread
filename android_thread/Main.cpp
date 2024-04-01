f5afba6415c1de082a29b49a27f8d4fb9a028b13
ore-ling <18633837894@163.com>
2024-04-01 10:06:36 +0800

    native thread

---
+++ android_thread/Main.cpp
@@ -0,0 +1,20 @@
+#define LOG_TAG "Main"
+ 
+#include <utils/Log.h>
+#include <utils/threads.h>
+#include "MyThread.h"
+ 
+using namespace android;
+ 
+int main()
+{
+	sp<MyThread>  thread = new MyThread;
+	thread->run("MyThread", PRIORITY_URGENT_DISPLAY);
+	while(1) {
+	   if (!thread->isRunning()) {
+		break;	   
+	    }	
+	}
+	ALOGD("main end");
+	return 0;
+}
